371ceb0437e90645c44c6dba3f2e6af4c3c0d48f
elnaxy12 <aryagilang533@gmail.com>
2025-10-20 22:55:25 +0700

    git init

--- resources/views/vila/beranda/index.blade.php
+++ resources/views/vila/beranda/index.blade.php
@@ -7,14 +7,31 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Beranda Vila Brooklyn</title>
 
+    <!-- Vite CSS dan JS -->
     @vite('resources/css/app.css')
+
+    <!-- Favicon -->
+    <link rel="icon" type="image/x-icon" href="{{ asset('images/icons/favicon/favicon.ico') }}">
+    <link rel="icon" type="image/png" href="{{ asset('images/icons/favicon/favicon-96x96.png') }}" sizes="96x96">
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('images/icons/favicon/apple-touch-icon.png') }}">
+    <meta name="apple-mobile-web-app-title" content="Vila Brooklyn">
+    <link rel="manifest" href="{{ asset('images/icons/favicon/site.webmanifest') }}">
+
+    <!-- Google Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+
+    <!-- Font Families -->
+    <link
+        href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Caveat:wght@400..700&family=Comic+Relief:wght@400;700&family=Dancing+Script:wght@400..700&family=Dongle&family=Montserrat:ital,wght@0,100..900;1,100..900&family=Open+Sans:ital,wght@0,300..800;1,300..800&family=Oswald:wght@200..700&family=Playwrite+DE+SAS:wght@100..400&family=Rock+3D&family=TASA+Explorer:wght@400..800&family=Winky+Rough:ital,wght@0,300..900;1,300..900&display=swap"
+        rel="stylesheet">
 </head>
 
 <body>
     <div class="full-navbar">
         <div class="container">
             <div class="col-1">
-                <img src="{{ asset('images/vila_brooklyn.jpg') }}" alt="Logo Vila" width="60">
+                <img src="{{ asset('images/vila-brooklyn.jpg') }}" alt="Logo Vila" width="60">
                 <p>Vila Brooklyn</p>
             </div>
             <div class="col-2">
